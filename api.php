8d9bb5384e6b13fb8073e7bdc6d80b9711017c34
Giammaria Biffi <102854627+beef-e@users.noreply.github.com>
2024-04-25 17:49:16 +0200

    Update include actors in film json object

--- api.php
+++ api.php
@@ -6,7 +6,18 @@ include_once 'db.php';
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if ($_SERVER['PATH_INFO'] == '/movies') {
         if (isset($_GET['titolo'])) {
-            echo json_encode(get_film($_GET['titolo']));
+            $film = json_encode(get_film($_GET['titolo']));
+            // TODO: Aggiungere al film anche gli attori
+            // facendo una query riguardante quel coso
+            $attori = json_encode(get_attori_by_film($film));
+
+            $json_film = json_decode($film);
+
+            $json_film["attori"]=json_decode($attori);
+      
+            echo json_encode($json_film);   
+
+            // echo $film;
         } else {
             echo json_encode(get_film(null));
         }
--- db.php
+++ db.php
@@ -12,7 +12,20 @@ function get_film($user_input)
   }
   $result = mysqli_query($conn, $sql);
   $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
   return $rows;
+  /*global $conn;
+  $sql = "";
+  if ($user_input == null) {
+    // $sql = "SELECT * FROM Film";
+    $sql = "SELECT * from Film JOIN Film_Attore as FA on FA.film_id=Film.id JOIN Film_Regista as FR on FR.film_id=Film.id JOIN Film_Genere as FG on FG.film_id=Film.id JOIN Attore as AA on AA.id=FA.attore_id JOIN Regista as RR on RR.id=FR.regista_id GROUP BY Film.titolo;";
+    } else {
+  // $sql = "SELECT * FROM Film WHERE titolo like '%$user_input%'";
+  $sql = "SELECT * from Film JOIN Film_Attore as FA on FA.film_id=Film.id JOIN Film_Regista as FR on FR.film_id=Film.id JOIN Film_Genere as FG on FG.film_id=Film.id JOIN Attore as AA on AA.id=FA.attore_id JOIN Regista as RR on RR.id=FR.regista_id WHERE titolo LIKE '%$user_input%' group by Film.titolo;";
+  }
+  $result = mysqli_query($conn, $sql);
+  $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  return $rows;*/
 }
 
 function get_attori($user_input)
@@ -55,4 +68,34 @@ function get_registi($user_input)
   $result = mysqli_query($conn, $sql);
   $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
   return $rows;
+}
+
+
+function get_attori_by_film($film)
+{
+  $film = json_decode($film, true);
+  $id_attori = [];
+  $attori = [];
+  foreach ($film as $entry) {
+    $id = $entry["id"];
+
+    global $conn;
+    $sql = "SELECT * FROM Film_Attore where film_id='$id'";
+    $result = mysqli_query($conn, $sql);
+    $film_attore = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    foreach ($film_attore as $coppia_id) {
+      array_push($id_attori, $coppia_id["attore_id"]);
+    }
+
+    foreach ($id_attori as $key) {
+      $sql = "SELECT * FROM Attore WHERE id=$key";
+      $result = mysqli_query($conn, $sql);
+      $current_actor = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+      array_push($attori, $current_actor);
+    }
+  }
+
+  return $attori;
 }
